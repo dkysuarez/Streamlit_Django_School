aeeade409b7d8d75042c45b6dbaa096273e5cc80
dkysuarez <dkysuarez1@gmail.com>
2024-07-16 00:42:08 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.image("The-school-visit-what-to-look-for-what-to-ask-1.jpg")
+st.title("Welcome to the main page of my project")
+st.write("This project combines Streamlit for the frontend and Django for the backend")
+
